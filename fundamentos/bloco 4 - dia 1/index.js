4208c5089373f829de19e501804fd8c5035c4b2b
icarols <icaro-live@hotmail.com>
2022-08-18 18:11:35 -0300

    primeiro dia de exercicios - bloco 4

---
+++ fundamentos/bloco 4 - dia 1/index.js
@@ -0,0 +1,18 @@
+// const myName = "Icaro Sousa";
+// const birthCity = "Juazeiro do Norte";
+// let birthYear = 1992;
+// birthYear = 2030;
+// birthCity = "Crato";
+
+// console.log(myName, birthCity, birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+patientId = "50";
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail, typeof patientAge); 
