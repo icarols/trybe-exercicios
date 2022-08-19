d53f9583afea5bd99850779cb07e2c8163a566d9
icarols <icaro-live@hotmail.com>
2022-08-19 19:15:57 -0300

    resolução ate exercicio 5 do bloco 4 dia 2

--- fundamentos/bloco 4 - dia 2/index.js
+++ fundamentos/bloco 4 - dia 2/index.js
@@ -1,2 +1,43 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [301, 9, 3, 19, 70, 8, 100, 227, 35, 27];
+let soma = 0;
+let media = 0;
+
+
+// for (let i = 0; i < numbers.length; i += 1){                     // exercicio 1 - imprime todos os itens de um array
+//     console.log(numbers[i]);
+// }
+  
+//-----------------------------------------------------------------------------------------------------------------------
+// for (let i = 0; i < numbers.length; i += 1){                     // exercicio 2 - soma dos itens de um array
+//      soma += numbers[i]; 
+// }
+// console.log(soma);
+
+// //-----------------------------------------------------------------------------------------------------------------------
+// for (let i = 0; i < numbers.length; i += 1){                     // exercicio 3 - media da soma dos itens do array
+//     media += numbers[i]; 
+// }
+// console.log(media / numbers.length);
+
+// //-----------------------------------------------------------------------------------------------------------------------
+// let valorFinal = (media / numbers.length);                       // exercicio 4 - verifica se media é maior que 20
+// if (valorFinal > 20){
+//     console.log("valor maior que 20");
+// }
+// else (console.log("valor menor ou igual a 20"));
+
+//-----------------------------------------------------------------------------------------------------------------------
+let test = 0;                                                       // exercicio 5 - verifica e imprime o maior valor do array
+let maiorNum = 0;
+for (let i = 0; i < numbers.length; i += 1){
+    test = numbers[i];
+    if (test > maiorNum){
+        maiorNum = test;
+    }
+    else (test = numbers[i]);
+}
+console.log(maiorNum);
+
+//-----------------------------------------------------------------------------------------------------------------------
+
 
