fb8cedf00e5366afc115aa3b48418916709e8e62
icarols <icaro-live@hotmail.com>
2022-08-23 15:20:35 -0300

    exercicios bloco 4 dia 4

--- fundamentos/bloco 4 - dia 1/index.js
+++ fundamentos/bloco 4 - dia 1/index.js
@@ -6,7 +6,7 @@
 
 // console.log(myName, birthCity, birthYear);
 
-
+//-----------------------------------------------------------------------------------------------------------------------
 
 // let patientId = 50;
 // let isEnrolled = true;
@@ -19,64 +19,144 @@
 
 // console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail, typeof patientAge); 
 
+//-----------------------------------------------------------------------------------------------------------------------
 
+// let a = -2;
+// let b = 4;
+// const c = 5;
+// const d = 3;
+// const e = 4;
+// let f = 60; 
+// let g = 60;
+// let h = 60;
+// let somaLados = f + g + h;
 
-let a = -2;
-let b = 4;
-const c = 5;
-const d = 3;
-const e = 4;
-let f = 60; 
-let g = 60;
-let h = 60;
-let somaLados = f + g + h;
+// let soma = a + b;                                    //exercicio 1 - adição, subtração, multiplicação, divisão e módulo
+// console.log(soma);
 
-let soma = a + b; //exercicio 1
-console.log(soma);
+// let subtr = a - b;
+// console.log(subtr);
 
-let subtr = a - b; //exercicio 1
-console.log(subtr);
+// let multi = a * b; 
+// console.log(multi);
 
-let multi = a * b; //exercicio 1
-console.log(multi);
+// let divis = a / b; 
+// console.log(divis);
 
-let divis = a / b; //exercicio 1
-console.log(divis);
+// let mod = a % b; 
+// console.log(mod);
 
-let mod = a % b; //exercicio 1
-console.log(mod);
+//-----------------------------------------------------------------------------------------------------------------------
 
-if (c > d){ //exercicio 2
-    console.log(d);}
-else{ 
-    console.log(e);} 
+// if (c > d){                                          //exercicio 2 - retorna o maior de dois numeros
+//     console.log(d);}
+// else{ 
+//     console.log(e);} 
 
-if (c > d && e){ //exercicio 3
-    console.log(c); }
-    else if (d > c && e){
-        console.log(d);}
-        else if (e > c && d){
-            console.log(e);}
-            else {
-                console.log("erro! troque os valores dos numeros")}
+//-----------------------------------------------------------------------------------------------------------------------
 
-if (a >= 1){ //exercicio 4
-    console.log("positive");}
-    else if (a < 0){ 
-        console.log("negative");}
-        else {
-            console.log("zero");}
+// if (c > d && e){                                     //exercicio 3 - retorna o maior de três numeros
+//     console.log(c); }
+//     else if (d > c && e){
+//         console.log(d);}
+//         else if (e > c && d){
+//             console.log(e);}
+//             else {
+//                 console.log("erro! troque os valores dos numeros")}
 
-if (f < 0){ //exercicio 5
-    console.log("erro! insira um valor positivo");
-}
-else if (g < 0){
-    console.log("erro! insira um valor positivo");
-}
-else if (h < 0){
-    console.log("erro! insira um valor positivo");
-}
-else if (somaLados == 180){
-    console.log(true);
-}
-else (console.log(false));
+//-----------------------------------------------------------------------------------------------------------------------
+
+// if (a >= 1){                                         //exercicio 4 - informa se o numero é positivo, negativo ou zero
+//     console.log("positive");}
+//     else if (a < 0){ 
+//         console.log("negative");}
+//         else {
+//             console.log("zero");}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// if (f < 0){                                          //exercicio 5 - verifica a soma de três angulos = 180 graus  
+//     console.log("erro! insira um valor positivo");
+// }
+// else if (g < 0){
+//     console.log("erro! insira um valor positivo");
+// }
+// else if (h < 0){
+//     console.log("erro! insira um valor positivo");
+// }
+// else if (somaLados == 180){
+//     console.log(true);
+// }
+// else (console.log(false));
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// let nomePeça = "DAMA".toLowerCase();                   //exercicio 6 - retorna os movimentos de uma peça de xadrez declarada
+
+// if (nomePeça == "rei"){
+//     console.log("Rei" + "-->" + "Vertical, horizontal e diagonal");
+// }
+// else if (nomePeça == "dama"){
+//     console.log("Dama" + "-->" + "Vertical, horizontal e diagonal");
+// }
+// else if (nomePeça == "torre"){
+//     console.log("Torre" + "-->" + "Vertical e horizontal");
+// }
+// else if (nomePeça == "bispo"){
+//     console.log("Bispo" + "-->" + "Diagonal");
+// }
+// else if (nomePeça == "cavalo"){
+//     console.log("Cavalo" + "-->" + "Movimento em L");
+// }
+// else if (nomePeça == "peão"){
+//     console.log("Peão" + "-->" + "Vertical");
+// }
+// else (console.log("Nome inválido! Informe um nome de peça válido."));
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// let vlNota = 101;                                       //exercicio 7 - converte valor numerico da nota para letras 
+
+// if (vlNota >= 90 && vlNota < 101){
+//     console.log("A");
+// }
+// else if (vlNota >= 80 && vlNota < 90){
+//     console.log("B");
+// }
+// else if (vlNota >= 70 && vlNota < 80){
+//     console.log("C");
+// }
+// else if (vlNota >= 60 && vlNota < 70){
+//     console.log("D");
+// }
+// else if (vlNota >= 50 && vlNota < 60){
+//     console.log("E");
+// }
+// else if (vlNota < 50 && vlNota > -1){
+//     console.log("F");
+// }
+// else (console.log("Erro! Insira umvalor entre 0 e 100."));
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// const n1 = 3;                                             //exercicio 8 - verifica se pelo menos um dos numeros é par e retorna true
+// const n2 = 5;
+// const n3 = 23;
+
+// if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0){
+//     console.log(true);
+// }
+// else (console.log(false));
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+// const n4 = 2;                                             //exercicio 9 - verifica se pelo menos um dos numeros é impar e retorna true
+// const n5 = 4;
+// const n6 = 7;
+
+// if (n4 % 2 !== 0 || n5 % 2 !== 0 || n6 % 2 !== 0){
+//     console.log(true);
+// }
+// else (console.log(false));
+
+//-----------------------------------------------------------------------------------------------------------------------
