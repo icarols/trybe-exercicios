9dee89d2f76d576e363118eafa80a4de5f4e6c9d
icarols <icaro-live@hotmail.com>
2022-10-06 18:36:16 -0300

    finalizando modulo 1

--- fundamentos/bloco 7 - dia 2/script.js
+++ fundamentos/bloco 7 - dia 2/script.js
@@ -1,82 +1,82 @@
 //soma de numero + string @@@---!!!---@@@
-// const sum = (value1, value2) => value1 + value2;
+const sum = (value1, value2) => value1 + value2;
 
-// console.log(sum(2, '3'));
+console.log(sum(2, '3'));
 
-//condição if para não existir string
-// const sum = (value1, value2) => {
-//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-//     return 'Os valores devem ser numéricos';
-//   }
-//   return value1 + value2;
-// };
+// condição if para não existir string
+const sum1 = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    return 'Os valores devem ser numéricos';
+  }
+  return value1 + value2;
+};
 
-// console.log(sum(2, '3'));
+console.log(sum1(2, '3'));
 
 //uso do throw para barrar aexecução da função caso os requisitos não sejam compativeis @@@---!!!---@@@
-// const sum1 = (value1, value2) => {
-//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-//     throw new Error('Os valores devem ser numéricos');
-//   }
-//   return value1 + value2;
-// };
+const sum3 = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+  return value1 + value2;
+};
 
-// console.log(sum1(2, '3'));
+console.log(sum3(2, '3'));
 
 //uso do try @@@---!!!---@@@
-// const verifyIsNumber = (value1, value2) => {
-//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-//     throw new Error('Os valores devem ser numéricos');
-//   }
-// };
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
 
-// const sum = (value1, value2) => {
-//   try {
-//     verifyIsNumber(value1, value2);
-//     return value1 + value2;
-//   } catch (error) {
-//     return error.message;
-//   }
-// };
+const sum4 = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
 
-// console.log(sum(2, 5));
+console.log(sum4(2, 5));
 
 //add uma propriedade a um objeto @@@---!!!---@@@
-// const customer1 = {
-//   firstName: 'Roberto',
-//   age: 22,
-//   job: 'Teacher',
-// };
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
 
-// console.log(customer1);
+console.log(customer1);
 
-// customer1.lastName = 'Faria';
-// console.log(customer1);
+customer1.lastName = 'Faria';
+console.log(customer1);
 
-// const customer2 = {
-//   firstName: 'Maria',
-//   age: 23,
-//   job: 'Medic',
-// };
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
 
-// console.log(customer2);
-// customer2['lastName'] = 'Silva';
-// console.log(customer2);
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log(customer2);
 
 //diferentes formas de associar chaves e valores a um objeto @@@---!!!---@@@
-// const customer = {
-//   firstName: 'Roberto',
-//   age: 22,
-//   job: 'Teacher',
-// };
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
 
-// let newKey = 'lastName';
-// const lastName = 'Ferreira';
-// customer[newKey] = lastName;
-// newKey = 'fullName';
-// const fullName = `${customer.firstName} ${customer.lastName}`;
-// customer[newKey] = fullName;
-// console.log(customer);
+let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
+console.log(customer);
 
 //listar as chaves de um objeto com for in @@@---!!!---@@@
 const coolestTvShow = {
@@ -88,9 +88,9 @@ const coolestTvShow = {
   seasons: 6,
 };
 
-// for (const i in coolestTvShow) {
-//   console.log(i);
-// }
+for (const i in coolestTvShow) {
+  console.log(i);
+}
 
 //método Object.keys para listar as chaves de um objeto retornando-as em um array @@@---!!!---@@@
 console.log(Object.keys(coolestTvShow));
@@ -123,3 +123,100 @@ listSkills(student1);
 
 console.log('Estudante 2');
 listSkills(student2);
+
+//listar os valores de cada chave do nosso objeto usando for in @@@---!!!---@@@
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+for (const property in coolestTvShow) {
+  console.log(coolestTvShow[property]);
+}
+
+//listar os valores de cada chave do nosso objeto usando Object.values @@@---!!!---@@@
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student));
+
+// Com Object.values
+console.log(listSkillsValuesWithValues(student));
+
+//usando o Object.entries para retornar um array de arrays, onde a primeira posição é a chave e a segunda o valor da chave @@@---!!!---@@@
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(let index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
+
+//usando Object.assign para copiar/clonar proprieadades de um objeto para outro modificando os dois ao mesmo tempo @@@---!!!---@@@
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+clone.name = 'Maria';
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+
+//usando Object.assign para copiar proprieadades de um objeto para outro novo @@@---!!!---@@@
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
